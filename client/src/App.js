5064bb6efb6ff0076fcf3920ca06ab700658ed07
Sasha Patsel <sasha8patsel@gmail.com>
2018-10-13 15:27:32 -0700

    add new autocmplete

--- client/src/App.js
+++ client/src/App.js
@@ -15,13 +15,18 @@ import './App.css';
 
 // helper functions
 import API from "./utils/API";
-import auto from "./utils/autocomplete"
+import Autosuggest from 'react-autosuggest';
+
+
+
 
 class App extends Component {
   state = {
     isLoggedIn: false,
     source: "",
-    currentFocus: 0
+    currentFocus: 0,
+    value: "",
+    suggestions: []
   }
 
   
@@ -31,7 +36,7 @@ class App extends Component {
 
   handleChange = async e => {
     
-    auto.autocomplete(e.target, sources, this);
+    // auto.autocomplete(e.target, sources, this);
     const {name, value} = e.target
     console.log(value)
     this.setState({
@@ -128,124 +133,7 @@ class App extends Component {
   }
 
 
-  // AUTOCOMPLETE =========================
 
-    autocomplete = (inp, arr, autocomp) => {
-      /*the autocomplete function takes two arguments,
-      the text field element and an array of possible autocompleted values:*/
-  
-      /*execute a function when someone writes in the text field:*/
-  
-      inp.addEventListener("input", async function(e) {
-          var a, b, i, val = this.value;
-          /*close any already open lists of autocompleted values*/
-          closeAllLists();
-          if (!val) { return false;}
-         await autocomp.setState({
-            currentFocus: -1
-          })
-          /*create a DIV element that will contain the items (values):*/
-          a = document.createElement("DIV");
-          a.setAttribute("id", this.id + "autocomplete-list");
-          a.setAttribute("class", "autocomplete-items");
-          /*append the DIV element as a child of the autocomplete container:*/
-          this.parentNode.appendChild(a);
-          /*for each item in the array...*/
-          for (i = 0; i < arr.length; i++) {
-            /*check if the item starts with the same letters as the text field value:*/
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-              /*create a DIV element for each matching element:*/
-              b = document.createElement("DIV");
-              /*make the matching letters bold:*/
-              b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-              b.innerHTML += arr[i].substr(val.length);
-              /*insert a input field that will hold the current array item's value:*/
-              b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-              /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
-                  /*insert the value for the autocomplete text field:*/
-                  inp.value = this.getElementsByTagName("input")[0].value;
-                  /*close the list of autocompleted values,
-                  (or any other open lists of autocompleted values:*/
-                  closeAllLists();
-              });
-              a.appendChild(b);
-            }
-          }
-      });
-      /*execute a function presses a key on the keyboard:*/
-      inp.addEventListener("keydown", async function(e) {
-        
-          var x = document.getElementById(this.id + "autocomplete-list");
-  
-          if (x) x = x.getElementsByTagName("div");
-          if (e.keyCode == 40) {
-            /*If the arrow DOWN key is pressed,
-            increase the currentFocus variable:*/
-           await autocomp.setState({
-             currentFocus: autocomp.state.currentFocus + 1
-           })
-            /*and and make the current item more visible:*/
-            addActive(x);
-          } else if (e.keyCode == 38) { //up
-            /*If the arrow UP key is pressed,
-            decrease the currentFocus variable:*/
-            autocomp.setState({
-              currentFocus: autocomp.state.currentFocus -1 
-            })
-            /*and and make the current item more visible:*/
-            addActive(x);
-          } else if (e.keyCode == 13) {
-            /*If the ENTER key is pressed, prevent the form from being submitted,*/
-            console.log(autocomp.state.source)
-            e.preventDefault();
-            if (autocomp.state.currentFocus > -1) {
-              /*and simulate a click on the "active" item:*/
-              if (x) x[autocomp.state.currentFocus].click();
-              autocomp.setState({
-                source: document.getElementById("myInput").value 
-              }, () => console.log(autocomp.state.source))
-            }
-          }
-      });
-      function addActive(x) {
-        /*a function to classify an item as "active":*/
-        if (!x) return false;
-        /*start by removing the "active" class on all items:*/
-        removeActive(x);
-        if (autocomp.state.currentFocus >= x.length) autocomp.state.currentFocus = 0;
-        if (autocomp.state.currentFocus < 0) autocomp.state.currentFocus = (x.length - 1);
-        /*add class "autocomplete-active":*/
-        x[autocomp.state.currentFocus].classList.add("autocomplete-active");
-      }
-      function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/
-        for (var i = 0; i < x.length; i++) {
-          x[i].classList.remove("autocomplete-active");
-        }
-      }
-      function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-        except the one passed as an argument:*/
-        var x = document.getElementsByClassName("autocomplete-items");
-        for (var i = 0; i < x.length; i++) {
-          if (elmnt != x[i] && elmnt != inp) {
-            x[i].parentNode.removeChild(x[i]);
-          }
-        }
-      }
-      /*execute a function when someone clicks in the document:*/
-      document.addEventListener("click", function (e) {
-          closeAllLists(e.target);
-      });
-    }
-
-  
-
-
-  
-
-  // ===================================
 
   setSource(src) {
     this.setState({
@@ -271,7 +159,62 @@ class App extends Component {
     })
   }
 
+
+  // AUTOCOMPLETE
+
+  // Teach Autosuggest how to calculate suggestions for any given input value.
+  getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : sources.filter(lang =>
+    lang.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+   getSuggestionValue = suggestion => suggestion;
+
+// Use your imagination to render suggestions.
+   renderSuggestion = suggestion => (
+  <div>
+    {suggestion}
+  </div>
+);
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
   render() {
+
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: 'Type a programming language',
+      value,
+      onChange: this.onChange
+    };
     return (
 
       // <Router>
@@ -303,8 +246,15 @@ class App extends Component {
 
       <form autocomplete="off" onSubmit={this.getNews}>
         <div class="autocomplete">
-        
-          <Input placeholder="Search News Source"  elementID="myInput" name="source" type="text" onChange={this.handleChange}/>
+        <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
+        inputProps={inputProps}
+      />
+          {/* <Input placeholder="Search News Source"  elementID="myInput" name="source" type="text" onChange={this.handleChange}/> */}
         </div>
         <input type="submit"/>
       </form>
